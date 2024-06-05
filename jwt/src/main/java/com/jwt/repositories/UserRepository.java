522ce1cbb706bb4a1629fb1dc40682ed5b8c6cdc
Tish30 <120072130+Tish30@users.noreply.github.com>
2024-06-05 23:39:41 +0530

    Add files via upload

---
+++ jwt/src/main/java/com/jwt/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.jwt.repositories;
+
+
+import com.jwt.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
---
+++ jwt/src/main/java/com/jwt/services/AuthenticationService.java
@@ -0,0 +1,56 @@
+package com.jwt.services;
+
+
+import com.jwt.repositories.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.jwt.dtos.RegisterUserDto;
+import com.jwt.dtos.LoginUserDto;
+
+import com.jwt.entity.User;
+
+
+@Service
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+
+
+    private final PasswordEncoder passwordEncoder;
+
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User signup(RegisterUserDto input) {
+        User user = new User()
+                .setFullName(input.getFullName())
+                .setEmail(input.getEmail())
+                .setPassword(passwordEncoder.encode(input.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public User authenticate(LoginUserDto input) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        input.getEmail(),
+                        input.getPassword()
+                )
+        );
+
+        return userRepository.findByEmail(input.getEmail())
+                .orElseThrow();
+    }
+}
